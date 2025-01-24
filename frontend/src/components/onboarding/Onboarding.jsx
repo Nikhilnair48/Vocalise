209eb73d2ea89ee6640d9e9f7d24194d14068d42
Nikhilnair48 <nikhil.nair48@gmail.com>
2025-01-24 12:48:46 +0530

    fix(frontend): chat ui and broadcasting fixed

--- frontend/src/components/onboarding/Onboarding.jsx
+++ frontend/src/components/onboarding/Onboarding.jsx
@@ -1,44 +1,63 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import io from "socket.io-client";
+import { useSocket } from "../../contexts/SocketContext";
 import "./index.css";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
-export const Onboarding = ({ setUserId }) => {
+export const Onboarding = ({ setUserId, setUserName }) => {
   const [username, setUsername] = useState("");
-  const [socket, setSocket] = useState(null);
+  const socket = useSocket();
   const navigate = useNavigate();
 
   useEffect(() => {
     const storedUserId = localStorage.getItem("userId");
     if (storedUserId) {
       setUserId(storedUserId);
+      setUserName(localStorage.getItem("username"));
       navigate("/chatrooms");
     }
-  }, [navigate, setUserId]);
+  }, [navigate, setUserId, setUserName]);
+
+  useEffect(() => {
+    if (socket) {
+      // Listen for messages from the server
+      socket.on("message", handleServerMessage);
+      socket.on("connect_error", handleConnectError);
+
+      return () => {
+        socket.off("message", handleServerMessage);
+        socket.off("connect_error", handleConnectError);
+      };
+    }
+  }, [socket]);
+
+  const handleServerMessage = (data) => {
+    if (data.text.startsWith("Welcome to the chat room")) {
+      const userId = localStorage.getItem("userId");
+      setUserId(userId);
+      setUserName(localStorage.getItem("username"));
+      navigate("/chatrooms");
+      socket.off("message", handleServerMessage);
+    }
+  };
+
+  const handleConnectError = (err) => {
+    console.error("Connection Error:", err);
+  };
 
   const handleJoinChat = () => {
-    if (!socket) {
-      const newSocket = io.connect("http://localhost:3000/chat");
-      setSocket(newSocket);
-
+    if (socket) {
       // Generate a new userId if not already set
       let userId = localStorage.getItem("userId");
       if (!userId) {
         userId = uuidv4();
         localStorage.setItem("userId", userId);
+        localStorage.setItem("username", username);
       }
 
-      newSocket.on("message", (data) => {
-        if (data.text.startsWith("Welcome to the chat room")) {
-          setUserId(userId);
-          navigate("/chatrooms");
-          newSocket.off("message");
-        }
-      });
-
-      // Send userId along with username to backend
-      newSocket.emit("join_room", { userId, username, room: "default" });
+      // Emit join_room event only once
+      socket.emit("join_room", { userId, username, room: "default" });
+      console.log(`Emitted 'join_room' for user ${username} in room 'default'.`);
     }
   };
 
